37db701d32e36975d8898d20a96704ff7f6c2dea
Vincent Beck <DV.Beck@student.han.nl>
2021-04-02 16:37:12 +0200

    added sample db and fetch all table data

--- src/main/java/io/aero/ServerMain.java
+++ src/main/java/io/aero/ServerMain.java
@@ -16,7 +16,7 @@ public class ServerMain {
 
         System.out.println("starting server...");
         Connection connection = null;
-        connection = DriverManager.getConnection("jdbc:sqlite:wms.db");
+        connection = DriverManager.getConnection("jdbc:sqlite:chinook.db");
         Connection finalConnection = connection;
 
         port(9010);
@@ -67,6 +67,36 @@ public class ServerMain {
 
             return new ObjectMapper().writeValueAsString(columns);
         });
+
+        get("/api/table/:tableName/all",(req, res) -> {
+
+            String tableName = req.params("tableName");
+
+            System.out.println(tableName);
+
+            List<Map<String, String>> columns = new ArrayList<>();
+
+            try {
+                Statement statement = finalConnection.createStatement();
+                statement.setQueryTimeout(30);  // set timeout to 30 sec.
+
+                ResultSet rs = statement.executeQuery("select * from "+tableName);
+                while(rs.next())
+                {
+                    Map<String, String> value = new HashMap<>();
+                    // read the result set
+                    for(int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {
+                        value.put(rs.getMetaData().getColumnName(i), rs.getString(i));
+                    }
+                    columns.add(value);
+                }
+            }catch (Exception e) {
+                System.out.println(e);
+            }
+
+            return new ObjectMapper().writeValueAsString(columns);
+        });
+
 
         get("/api/table/:tableName/schema",(req, res) -> {
 
